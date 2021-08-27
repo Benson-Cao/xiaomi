919079218f15a2fa270e7f0b73dc6da4e80b9bbe
洛雨尘风 <2381386908@qq.com>
2021-08-27 18:02:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const prevBtn=document.querySelector('.prev')
+const nextBtn=document.querySelector('.next')
+const slides=document.querySelector('.slides')
+const ul=slides.querySelector('ul')
+const lis=ul.querySelectorAll('li')
+var goNext=function(){
+    
+}
+
+nextBtn.addEventListener('click',()=>{
+    goNext();
+})
+
