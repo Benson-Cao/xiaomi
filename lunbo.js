3c2849cc4af99f84d6d367e2180b8e64f99ad47e
Luo-6-feng <72428406+Luo-6-feng@users.noreply.github.com>
2021-08-28 19:32:31 +0800

    Delete lunbo.js

--- lunbo.js
+++
@@ -1,8 +0,0 @@
-
-const div=document.querySelector('div');
-const prev=document.querySelector('.prev');
-const next=document.querySelector('.next');
-
-const ul=document.querySelector('ul');
-const lis= document.querySelectorAll('li');
-const ol= document.querySelector('ol');
